0bda3f9bce3ac2908a315bae72a465c24d52066a
DESKTOP-AJ53M86\Александр <aleksdark13@gmail.com>
2018-04-08 16:19:18 +0300

    add images

--- app/js/findem.js
+++ app/js/findem.js
@@ -3,15 +3,15 @@ var jQuery = require('jquery');
 
 jQuery(document).ready(function($) {
 
-  	var myWidth = 0;
-        myWidth = window.innerWidth;
-        jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
-        jQuery(window).resize(function(){
-            var myWidth = 0;
-            myWidth = window.innerWidth;
-            jQuery('#size').remove();
-            jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
-        });
+  	// var myWidth = 0;
+   //      myWidth = window.innerWidth;
+   //      jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
+   //      jQuery(window).resize(function(){
+   //          var myWidth = 0;
+   //          myWidth = window.innerWidth;
+   //          jQuery('#size').remove();
+   //          jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
+   //      });
 
             /* Responsive meni */
 
