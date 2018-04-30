883e3d8d296e71c9ebebbc9c3885222c9130a70a
DESKTOP-AJ53M86\Александр <aleksdark13@gmail.com>
2018-04-30 21:47:54 +0300

    add responsive home, project, about, contact, one page

--- app/js/findem.js
+++ app/js/findem.js
@@ -7,15 +7,15 @@ var lightSlider= require('lightslider');
 
 jQuery(document).ready(function($) {
 
-  	// var myWidth = 0;
-   //      myWidth = window.innerWidth;
-   //      jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
-   //      jQuery(window).resize(function(){
-   //          var myWidth = 0;
-   //          myWidth = window.innerWidth;
-   //          jQuery('#size').remove();
-   //          jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
-   //      });
+  	var myWidth = 0;
+        myWidth = window.innerWidth;
+        jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
+        jQuery(window).resize(function(){
+            var myWidth = 0;
+            myWidth = window.innerWidth;
+            jQuery('#size').remove();
+            jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:9999;color:#fff;">Width = '+myWidth+'</div>');
+        });
 
             /* Responsive meni */
 
